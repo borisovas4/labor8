feb0c63da2869e390bc75a6cb9cee5b3e0f893af
borisovas4 <81374549+borisovas4@users.noreply.github.com>
2021-12-02 15:31:15 +0900

    Update Class.java

--- src/Class.java
+++ src/Class.java
@@ -1,12 +1,12 @@
 public class Class {
     static public void main(String[] args) {
-        Student student = new Student("asd", "student", 23);
+        Student student = new Student("asd", "student", 18);
         student.walk();
         student.run();
         student.voice();
         student.learn();
         System.out.println(student.toString());
-        Teacher teacher = new Teacher("das", "teacher", 43);
+        Teacher teacher = new Teacher("das", "teacher", 45);
         teacher.walk();
         teacher.run();
         teacher.voice();
